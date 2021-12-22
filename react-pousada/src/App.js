0916591ec083acf04ea200b6440703c7a12e649f
Davi <seauott@gmail.com>
2021-12-22 17:23:12 -0300

    Rota Reserva

--- react-pousada/src/App.js
+++ react-pousada/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import Home from './components/Home/Home';
 import Cadastro from './components/Cadastro/Cadastro';
 import Pagamento from './components/Pagamento/Pagamento';
+import Reserva from './components/Reserva/Reserva';
 //import Eventos from './pages/Eventos'
 //import './App.css';
 import {BrowserRouter as Router,Route, Routes} from 'react-router-dom' ;
@@ -13,6 +14,7 @@ function App() {
             <Route exact path="/" element={<Home/>}/>
             <Route exact path="/cadastro" element={<Cadastro/>}/>
             <Route exact path="/pagamemto" element={<Pagamento/>}/>
+            <Route exact path="/reserva" element={<Reserva/>}/>
           </Routes>
       </Router>
   
